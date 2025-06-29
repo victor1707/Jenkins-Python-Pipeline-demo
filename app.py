db8417c49d7fe84d3a927ae0d2dff4556f864706
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2025-06-29 10:33:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 # app.py
+from flask import Flask 
+import os 
+app = Flask(__name__) 
+@app.route('/')
 
 def add(a, b):
     return a + b
@@ -7,7 +11,12 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def hello(): 
+    return ('\nHello from Container World!-Deployment using Docker Image \n\n')
+
 
 if __name__ == "__main__":
     print("Addition:", add(5, 3))
     print("Subtraction:", subtract(5, 3))
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
